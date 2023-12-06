5249170e8a7a1d5111cc6acc58a87d0620bbd690
Farouk Farag <F.Farag@seersolutionz.com>
2023-12-06 18:02:03 +0400

    Implement Error Handling

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,5 +1,8 @@
+using Api.Errors;
+using Api.Filters;
 using Application;
 using Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,10 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.RegisterApplication();
 builder.Services.RegisterInfrastructure(builder.Configuration);
 
+//builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ProblemDetailsFactory, CustomProblemDetailsFactory>();
 
 var app = builder.Build();
 
@@ -20,6 +26,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//app.UseMiddleware<ErrorHandlingMiddleware>();
+
+app.UseExceptionHandler("/error");
 
 app.UseHttpsRedirection();
 
